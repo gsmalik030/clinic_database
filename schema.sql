439443fc95cccf341fb442d7e9b8affd0f34dda1
Ghulam Subbhani <gsmalik030@gmail.com>
2023-04-04 16:00:12 +0200

    Created invoices and invoice_items table

--- schema.sql
+++ schema.sql
@@ -11,4 +11,34 @@ CREATE TABLE IF NOT EXISTS medical_histories (
 	FOREIGN KEY (patients_id)
 	REFERENCES patients(id),
 	status VARCHAR(50)
-)
+);
+
+CREATE TABLE IF NOT EXISTS treatments(
+	id SERIAL PRIMARY KEY,
+	type VARCHAR(50) NOT NULL,
+	name VARCHAR (50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS invoices(
+	id SERIAL PRIMARY KEY,
+	total_amount DECIMAL(5, 2) NOT NULL,
+	genrated_at TIMESTAMP,
+	payed_at TIMESTAMP,
+	medical_histories_id INT REFERENCES medical_histories(id)
+);
+
+CREATE TABLE IF NOT EXISTS invoice_items(
+	id SERIAL PRIMARY KEY,
+	unit_price DECIMAL (5, 2) NOT NULL,
+	quantity INT NOT NULL DEFAULT 0,
+	total_price DECIMAL (5, 2) NOT NULL,
+	invoices_id INT REFERENCES invoices(id),
+	treatments_id INT REFERENCES treatments(id)
+);
+
+CREATE TABLE IF NOT EXISTS treatments_has_medical_histories(
+	treatments_id INT REFERENCES treatments(id),
+	medical_histories_id INT REFERENCES medical_histories(id)
+);
+
+
